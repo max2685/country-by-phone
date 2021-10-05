d1d3d4c438efa750ae01459ffea3a1f6c269c6ca
maksims.tolkacovs <maksims.tolkacovs@neotech.lv>
2021-10-05 10:05:40 +0300

    remove some annotations

--- src/main/java/com/neotech/countrybyphone/app/model/error/ErrorModel.java
+++ src/main/java/com/neotech/countrybyphone/app/model/error/ErrorModel.java
@@ -1,11 +1,9 @@
 package com.neotech.countrybyphone.app.model.error;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@Builder
 @RequiredArgsConstructor
 public class ErrorModel {
     private final String errorMessage;
